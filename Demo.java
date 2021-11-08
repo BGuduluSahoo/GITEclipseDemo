391a4e1ccc3a409b3083a067a770e11834f59d3b
BabluIT <iasbablu1998@gmail.com>
2021-11-08 20:07:11 +0530

    hjhg

---
+++ Demo.java
@@ -0,0 +1,9 @@
+public class Demo{
+      public static void main(String[] args){
+        int b = 10;
+        System.out.println(b);
+
+      }
+
+
+}
---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test{
+      public static void main(String[] args){
+        int c = 10;
+            int b = 20;
+        System.out.println(c);
+
+      }
+
+
+}
